ebebe50363f059c4bae9f858495a9f9621b0e401
rjuang <rjuang@github.com>
2021-07-17 15:42:19 -0700

    Fix background coloring of lights when visual metronome enabled.

--- arturia_metronome.py
+++ arturia_metronome.py
@@ -1,5 +1,8 @@
 from arturia_leds import ArturiaLights
 
+import arturia_leds
+
+import channels
 import config
 import playlist
 import ui
@@ -29,7 +32,17 @@ class VisualMetronome:
             # Disable metronome if configured to correlate to metronome toggle and is disabled.
             return
 
-        lights = ArturiaLights.ZeroMatrix()
+        should_color_pads = ((not arturia_leds.ESSENTIAL_KEYBOARD and config.ENABLE_MK2_COLORIZE_PAD_LIGHTS) or
+                             (arturia_leds.ESSENTIAL_KEYBOARD and config.ENABLE_COLORIZE_BANK_LIGHTS))
+        zero_val = 0
+        on_val = ArturiaLights.LED_ON
+        if should_color_pads:
+            channel_idx = channels.selectedChannel()
+            color_val = channels.getChannelColor(channel_idx)
+            zero_val = ArturiaLights.fadedColor(color_val)
+            on_val = ArturiaLights.fullColor(color_val)
+
+        lights = ArturiaLights.ZeroMatrix(zero=zero_val)
         current_bst_position = (playlist.getVisTimeBar(),
                                 playlist.getVisTimeStep(),
                                 playlist.getVisTimeTick())
@@ -52,11 +65,11 @@ class VisualMetronome:
         if value != 0:
             row = self._bar_count % num_rows
             col = self._beat_count % num_cols
-            lights[row][col] = ArturiaLights.LED_ON
+            lights[row][col] = on_val
             two_step = self._beat_count % 2 == 0
 
             if config.ENABLE_PAD_METRONOME_LIGHTS:
-                self._lights.SetPadLights(lights)
+                self._lights.SetPadLights(lights, rgb=should_color_pads)
 
             if config.ENABLE_TRANSPORTS_METRONOME_LIGHTS:
                 self._lights.SetLights({
