ac069132d31c8757e43b2bd3b35027c628c3fee6
rjuang <rjuang@github.com>
2021-07-28 01:46:57 -0700

    Updating version number.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # User-settable settings to alter behavior of keyboard
 
 # Number representing the last change date
-CHANGE_DATE = 2021_07_27_0102
+CHANGE_DATE = 2021_07_28_0146
 
 # Set to True to allow drum pads to light up as a metronome indicator.
 ENABLE_PAD_METRONOME_LIGHTS = True
