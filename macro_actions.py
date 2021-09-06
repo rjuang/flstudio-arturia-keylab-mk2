432b86ae1be1ee9201f5edcc2e9220796cbd2952
rjuang <rjuang@github.com>
2021-09-06 15:09:17 -0700

    Adding a "hacky" way to reveal Browser. It works but is considered hacky because it can break on UI changes. This needs to be version guarded and tested on different platforms.

--- macro_actions.py
+++ macro_actions.py
@@ -26,6 +26,49 @@ RIGHT_BUTTON = 32
 # Bitmask for save button (song/pattern button)
 SAVE_BUTTON = 64
 
+# Number of times the left arrow needs to be clicked to reach the menu item.
+# NOTE: Do NOT use "right" arrow because that key can enter the menu.
+_MENU_LEFT_COUNT = {
+    'file': 0,
+    'edit': 7,
+    'add': 6,
+    'patterns': 5,
+    'view': 4,
+    'options': 3,
+    'tools': 2,
+    'help': 1,
+}
+
+# Number of times the down arrow needs to be clicked to reach the menu item.
+_MENU_DOWN_COUNT = {
+    # View menu
+    'playlist': 1,
+    'piano roll': 2,
+    'channel rack': 3,
+    'mixer': 4,
+    'browser': 5,
+    'project picker': 6,
+    'plugin picker': 7,
+    'tempo tapper': 8,
+    'touch controller': 9,
+    'script output': 10,
+    'toolbars': 11,
+    'tests': 12,
+    'plugin performance monitor': 13,
+    'close all windows': 14,
+    'close all plugin windows': 15,
+    'close all unfocused windows': 16,
+    'align all channel editors': 17,
+    'arrange windows': 18,
+    'background': 19,
+    'undo history': 20,
+    'patterns': 21,
+    'generators in use': 22,
+    'effects in use': 23,
+    'remote control': 24,
+    'plugin database': 25,
+}
+
 
 class Actions:
     """Class that houses all action functions available.
@@ -60,8 +103,16 @@ class Actions:
         if ui.getVisible(midi.widBrowser):
             ui.hideWindow(midi.widBrowser)
         else:
-            ui.showWindow(midi.widBrowser)
-            ui.setFocused(midi.widBrowser)
+            if SCRIPT_VERSION <= 12:  # As of 20.8.4 and earlier, widBrowser doesn't properly show the browser window.
+                # Hacky way to toggle browser window.
+                # Public rant: FL Studio does not have a way to send modifier keys in their shortcut, nor any way to
+                # access menu items via API. This is a hacky way to do this.
+                Actions._open_app_menu()
+                Actions._navigate_to_menu('view', 'browser')
+                Actions._press_enter()
+            else:
+                ui.showWindow(midi.widBrowser)
+                ui.setFocused(midi.widBrowser)
 
     @staticmethod
     def toggle_plugin_visibility():
@@ -86,3 +137,27 @@ class Actions:
         """Not assigned"""
         # Do nothing
         pass
+
+    @staticmethod
+    def _press_enter():
+        transport.globalTransport(midi.FPT_Enter, 1)
+
+    @staticmethod
+    def _open_app_menu():
+        # FPT_Menu needs to be triggered when channel rack in focus in order to bring up the app menu.
+        # Save the visibility state of the channel rack and restore at the end.
+        channel_rack_visible = ui.getVisible(midi.widChannelRack)
+        if not channel_rack_visible:
+            ui.showWindow(midi.widChannelRack)
+        ui.setFocused(midi.widChannelRack)
+
+        transport.globalTransport(midi.FPT_Menu, 1)
+        if not channel_rack_visible:
+            ui.hideWindow(midi.widChannelRack)
+
+    @staticmethod
+    def _navigate_to_menu(menu, item):
+        for _ in range(_MENU_LEFT_COUNT[menu]):
+            transport.globalTransport(midi.FPT_Left, 1)
+        for _ in range(_MENU_DOWN_COUNT[item]):
+            transport.globalTransport(midi.FPT_Down, 1)
