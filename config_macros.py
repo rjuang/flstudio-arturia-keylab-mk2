8bf26a14458c3afaf4ca84b707f0142cc117a11f
rjuang <rjuang@github.com>
2021-09-18 23:10:16 -0700

    Adding missing constants to import list.

--- config_macros.py
+++ config_macros.py
@@ -1,5 +1,7 @@
 from macro_actions import Actions
+from macro_actions import ENCODER1, ENCODER2, ENCODER3, ENCODER4, ENCODER5, ENCODER6, ENCODER7, ENCODER8, ENCODER9
 from macro_actions import LOOP_BUTTON, LEFT_BUTTON, REC_BUTTON, RIGHT_BUTTON, PLAY_BUTTON, SAVE_BUTTON, STOP_BUTTON
+from macro_actions import NAV_WHEEL
 
 OVERRIDE_MACROS = {
     # Override the default macro assignment with a custom action.
@@ -11,4 +13,8 @@ OVERRIDE_MACROS = {
     # (PLAY_BUTTON, 4): Actions.redo,
     # (SAVE_BUTTON, 5): Actions.redo,
     # (STOP_BUTTON, 6): Actions.redo,
+    #
+    # (LOOP_BUTTON, NAV_WHEEL): Actions.horizontal_zoom,
+    # (LOOP_BUTTON, ENCODER1): Actions.strip_jog,
+    # (LOOP_BUTTON, ENCODER2): Actions.scale_input_by(2, Actions.strip_jog),
 }
